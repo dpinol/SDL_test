9095b8ab976877a81e29f341696f9d0ccf8d2c55
dani <github@dpinol.com>
2014-03-01 15:59:46 +0100

    paints background

--- GameState.cpp
+++ GameState.cpp
@@ -32,7 +32,7 @@ void GameState::update()
   {
       for(int i = 0; i < m_gameObjects.size(); i++)
       {
-          m_gameObjects[i]->draw();
+          m_gameObjects[i]->update();
       }
   }
 }
