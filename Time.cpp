ee6ad7520fc1233c56c0848df9d8bc12e9f13435
dani <github@dpinol.com>
2014-03-03 11:07:11 +0100

    moved to model

--- Time.cpp
+++
@@ -1,9 +0,0 @@
-/**************************************************************************
-** Qt Creator license header template
-**   Special keywords: dpinol 28/02/2014 2014
-**   Environment variables:
-**   To protect a percent sign, use '%'.
-**************************************************************************/
-
-#include "Time.h"
-
